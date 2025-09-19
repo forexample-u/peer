bbd777887e43f8b126244f5f3c50158a7eef60e9
forexample-u <jone.cina2016@yandex.ru>
2025-09-19 18:38:59 +0300

    fix: delete file endpoint

--- Peer/Controllers/PeerController.cs
+++ Peer/Controllers/PeerController.cs
@@ -43,33 +43,4 @@ public class PeerController : ControllerBase
             }
         }
     }
-
-    [HttpGet("file/{id}")]
-    public IActionResult File(string id)
-    {
-        if (!System.IO.File.Exists(Path.Combine("wwwroot", "peer", id)))
-        {
-            return NotFound();
-        }
-        string filename = string.Concat(id.Split("_").Skip(1));
-        string contentType = GetContentType(Path.GetExtension(filename));
-        Response.Headers.Add("Content-Disposition", "inline");
-        return File(Path.Combine("peer", id));
-    }
-
-    private string GetContentType(string ext)
-    {
-        string type = "application/octet-stream";
-        if (ext == ".html") { type = "text/html"; }
-        if (ext == ".json") { type = "application/json"; }
-        if (ext == ".jpeg") { type = "image/jpeg"; }
-        if (ext == ".txt") { type = "text/plain"; }
-        if (ext == ".jpg") { type = "image/jpeg"; }
-        if (ext == ".png") { type = "image/png"; }
-        if (ext == ".gif") { type = "image/gif"; }
-        if (ext == ".mp4") { type = "video/mp4"; }
-        if (ext == ".mp3") { type = "audio/mpeg"; }
-        if (ext == ".pdf") { type = "application/pdf"; }
-        return type;
-    }
 }
