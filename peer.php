a7f315e4d89dd8fcaa2f9c8d05e7584b680e85cc
forexample-u <jone.cina2016@yandex.ru>
2025-10-04 13:24:26 +0300

    fix: return url

--- peer.php
+++ peer.php
@@ -1,62 +1,49 @@
 <?php
-$uploadFolder = __DIR__ . '/peer';
-$segments = explode('/', trim(substr(strtok($_SERVER['REQUEST_URI'], '?'), strlen('/peer')), '/'));
-if (strpos($segments[0], "php") !== false) {
-    array_shift($segments);
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+$uploadFolder = __DIR__ . DIRECTORY_SEPARATOR . 'peer';
+$segments = explode('/', trim(strtok($_SERVER['REQUEST_URI'], '?'), '/'));
+$addUrl = strpos($segments[0] ?? '', "php") ? ("/" . array_shift($segments)) : "";
+if (count($segments) !== 2 || $segments[0] !== "peer") {
+    http_response_code(404);
+    exit;
 }
-if ($segments[0] === "peer") {
-    array_shift($segments);
-}
-header('Content-Type: application/json; charset=utf-8');
-header("Access-Control-Allow-Origin: *");
-switch ($segments[0]) {
-    case 'upload':
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (!isset($_FILES['file']) || $_FILES['file']['error'] !== UPLOAD_ERR_OK) {
-                http_response_code(400);
-                echo "";
-                break;
-            }
-            if (!is_dir($uploadFolder)) {
-                mkdir($uploadFolder, 0777, true);
-            }
-            $filename = basename($_FILES['file']['name']);
-            $i = 0;
-            while (true) {
-                $fileid = $i . '_' . $filename;
-                $i += 1;
-                $filepath = $uploadFolder . DIRECTORY_SEPARATOR . $fileid;
-                if (file_exists($filepath)) {
-                    continue;
-                }
-                if (move_uploaded_file($_FILES['file']['tmp_name'], $filepath)) {
-                    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-                    $url = $protocol . $_SERVER['HTTP_HOST'] . "/peer/" . $fileid;
-                    http_response_code(200);
-                    echo $url;
-                    break;
-                } else {
-                    if (file_exists($filepath)) {
-                        continue;
-                    }
-                    http_response_code(500);
-                    echo "";
-                    break;
-                }
-            }
-        } else {
-            http_response_code(405);
-        }
-        break;
-
-    default:
-        $filepath = $uploadFolder . "/" . urldecode($segments[0]);
+if ($segments[1] === "upload" && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!isset($_FILES['file']) || $_FILES['file']['error'] !== UPLOAD_ERR_OK) {
+        http_response_code(400);
+        exit;
+    }
+    if (!is_dir($uploadFolder)) {
+        mkdir($uploadFolder, 0777, true);
+    }
+    $filename = basename($_FILES['file']['name']);
+    $i = 0;
+    while (true) {
+        $fileid = $i . '_' . $filename;
+        $filepath = $uploadFolder . DIRECTORY_SEPARATOR . $fileid;
+        $i += 1;
         if (file_exists($filepath)) {
-            header('Content-Type: ' . mime_content_type(urldecode($segments[0])));
-            echo file_get_contents($filepath);
-        } else {
-            http_response_code(404);
+            continue;
         }
-        break;
+        if (move_uploaded_file($_FILES['file']['tmp_name'], $filepath)) {
+            $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+            http_response_code(200);
+            echo $protocol . $_SERVER['HTTP_HOST'] . $addUrl . "/peer/" . $fileid;
+            break;
+        }
+        if (!file_exists($filepath)) {
+            http_response_code(500);
+            break;
+        }
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $filepath = $uploadFolder . DIRECTORY_SEPARATOR . urldecode($segments[1]);
+    if (file_exists($filepath)) {
+        header('Content-Type: ' . mime_content_type(urldecode($segments[1])));
+        echo file_get_contents($filepath);
+    } else {
+        http_response_code(404);
+    }
 }
 ?>
