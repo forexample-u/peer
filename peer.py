29adcbc4d71b9fdb7d681c8fa28a318f68ec1f0e
forexample-u <jone.cina2016@yandex.ru>
2025-10-05 15:20:29 +0300

    feat: add new index url

--- peer.py
+++ peer.py
@@ -36,15 +36,15 @@ def upload():
                 continue
             return "", 500
 
-@app.route('/')
+@app.route('/peer/index')
 def index():
     return """<!DOCTYPE html><html><head><style>body { background:#121212; } input, a { font-size:40px; display:block; color:#fff; }</style>
-</head><body><input type="file" id="in" onchange="upload();" />
-<script>async function upload() {
-  const fd = new FormData(); fd.append("file", document.getElementById("in").files[0]);
-  const url = await (await fetch(location.origin + "/peer/upload", { method: "POST", body: fd })).text();
-  document.body.innerHTML += '<a target="_blank" href="' + url + '">' + url + "</a>";
-}</script></body></html>""", 200
+         </head><body><input type="file" id="in" onchange="upload();" />
+          <script>async function upload() {
+            const fd = new FormData(); fd.append("file", document.getElementById("in").files[0]);
+            const url = await (await fetch(location.origin + "/peer/upload", { method: "POST", body: fd })).text();
+            document.body.innerHTML += '<a target="_blank" href="' + url + '">' + url + "</a>";
+          }</script></body></html>""", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
