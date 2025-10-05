ae8195b94eb10558195a5374200339b2848f005b
forexample-u <jone.cina2016@yandex.ru>
2025-10-05 14:03:47 +0300

    feat: add start page

--- peer.py
+++ peer.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory, current_app
+from flask import Flask, request, send_file, current_app
 import os
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ def after_request(response):
 
 @app.route('/peer/<path:filename>')
 def static_file(filename):
-    return send_from_directory('static', os.path.join('peer', filename))
+    return send_file(os.path.join('static', os.path.join('peer', filename)))
 
 @app.route('/peer/upload', methods=['POST'])
 def upload():
@@ -21,23 +21,30 @@ def upload():
         return "", 400
     upload_folder = os.path.join(current_app.root_path, 'static', 'peer')
     os.makedirs(upload_folder, exist_ok=True)
-    file_length = len(file.read())
-    file.seek(0)
     i = 0
     while True:
         file_id = f"{i}_{file.filename}"
-        i += 1
         file_path = os.path.join(upload_folder, file_id)
+        i += 1
         if os.path.exists(file_path):
             continue
         try:
             file.save(file_path)
-            url = f"{request.host_url.rstrip('/')}/peer/{file_id}"
-            return url, 200
+            return f"{request.host_url.rstrip('/')}/peer/{file_id}", 200
         except Exception:
             if os.path.exists(file_path):
                 continue
             return "", 500
+
+@app.route('/')
+def index():
+    return """<!DOCTYPE html><html><head><style>body { background:#121212; } input, a { font-size:40px; display:block; color:#fff; }</style>
+</head><body><input type="file" id="in" onchange="upload();" />
+<script>async function upload() {
+  const fd = new FormData(); fd.append("file", document.getElementById("in").files[0]);
+  const url = await (await fetch(location.origin + "/peer/upload", { method: "POST", body: fd })).text();
+  document.body.innerHTML += '<a target="_blank" href="' + url + '">' + url + "</a>";
+}</script></body></html>""", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
