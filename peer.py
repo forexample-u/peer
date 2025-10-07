4a070fff32f55ca8dae7fefa6fd96617e7165d57
forexample-u <jone.cina2016@yandex.ru>
2025-10-07 21:19:58 +0300

    feat: add start page /

--- peer.py
+++ peer.py
@@ -36,15 +36,9 @@ def upload():
                 continue
             return "", 500
 
-@app.route('/peer/index')
+@app.route('/')
 def index():
-    return """<!DOCTYPE html><html><head><style>body { background:#121212; } input, a { font-size:40px; display:block; color:#fff; }</style>
-          </head><body><input type="file" id="in" onchange="upload();" />
-          <script>async function upload() {
-            const fd = new FormData(); fd.append("file", document.getElementById("in").files[0]);
-            const url = await (await fetch(location.origin + "/peer/upload", { method: "POST", body: fd })).text();
-            document.body.innerHTML += '<a target="_blank" href="' + url + '">' + url + "</a>";
-          }</script></body></html>""", 200
+    return send_file(os.path.join('static', 'index.html'))
 
 if __name__ == '__main__':
     app.run(debug=True)
