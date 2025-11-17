3b14c0c76dcf35a75b19afc0f804416707e41094
forexample-u <98162342+forexample-u@users.noreply.github.com>
2025-11-17 18:52:36 +0300

    feat: try change host to public

--- peer.py
+++ peer.py
@@ -57,4 +57,4 @@ def index():
     return send_file(os.path.join('static', 'peerdata', 'index.html'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=False)
